992fabad3239c3959349203b40562bf18aed09f4
Sachin <sachin3434z@gmail.com>
2026-08-07 23:02:26 +0530

    Time: 47 ms (33.06%), Space: 85.8 MB (92.6%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-       if (nums.size() < 2)
-            return 0;
-
+       if (nums.size() < 2) return 0;
         int maxGap = 0;
-
-        // Step 1: Sort the array.
         sort(nums.begin(), nums.end());
 
-        // Step 2: Compare adjacent elements and track the maximum gap.
         for (int i = 0; i < nums.size() - 1; i++) {
-
             int newDiff = nums[i + 1] - nums[i];
-
             if (newDiff > maxGap)
                 maxGap = newDiff;
         }
-
         return maxGap;  
     }
 };
